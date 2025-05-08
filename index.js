bfd817f5f47a84aa42ae5a766a7dfc094ee72234
pishrovest <pishrovest@gmail.com>
2025-05-07 23:20:44 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import { Platform } from 'react-native';
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-if (Platform.OS === 'web') {
-  require('./app.web');
-} else {
-  registerRootComponent(App);
-}
